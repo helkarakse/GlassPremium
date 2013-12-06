2e89a41f522738eea335df12ddae77b864a05add
helkarakse <nimcuron@gmail.com>
2013-12-07 04:49:32 +0800

    Changed parser call to rssParser

--- src/glass.lua
+++ src/glass.lua
@@ -250,7 +250,7 @@ local function drawRss(inputX, inputY)
 	
 	local j = 0
 	for key, value in pairs(data) do
-		local title, link, desc, pubDate, guid = parser.parseItem(value)
+		local title, link, desc, pubDate, guid = rssParser.parseItem(value)
 		table.insert(rssArray, bridge.addText(inputX, inputY + (lineMultiplier * j), title, colors.white).setScale(size.small))
 		table.insert(rssArray, bridge.addText(inputX + 100, inputY + (lineMultiplier * j), date, colors.white).setScale(size.small))
 		j = j + 1
