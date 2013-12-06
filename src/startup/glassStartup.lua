db22b8af88213e0b01234a3df73398309e05956f
helkarakse <nimcuron@gmail.com>
2013-12-07 01:57:35 +0800

    Added rssParser and libXml to the startup

--- src/startup/glassStartup.lua
+++ src/startup/glassStartup.lua
@@ -12,7 +12,9 @@
 local fileArray = {
 	{link = "https://raw.github.com/helkarakse/LuaLibs/master/src/common/functions.lua", file = "functions"},
 	{link = "https://raw.github.com/helkarakse/LuaLibs/master/src/libs/libJson.lua", file = "json"},
-	{link = "https://raw.github.com/helkarakse/LuaLibs/master/src/parser/tickParser.lua", file = "parser"},
+	{link = "https://raw.github.com/helkarakse/LuaLibs/master/src/libs/libXml.lua", file = "xml"},
+	{link = "https://raw.github.com/helkarakse/LuaLibs/master/src/parser/tickParser.lua", file = "tickParser"},
+	{link = "https://raw.github.com/helkarakse/LuaLibs/master/src/parser/rssParser.lua", file = "rssParser"},
 	{link = "https://raw.github.com/helkarakse/GlassPremium/master/src/glass.lua", file = "glass"}
 }
 
