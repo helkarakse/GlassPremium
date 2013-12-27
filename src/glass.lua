5982543291638de669353b4646e0533309fc10c1
helkarakse <nimcuron@gmail.com>
2013-12-27 11:16:40 +0800

    Updated url for glass

--- src/glass.lua
+++ src/glass.lua
@@ -23,10 +23,11 @@ local string = string
 
 -- Variables
 local dimId = string.sub(os.getComputerLabel(), 1, 1)
+local server = string.lower(string.sub(os.getComputerLabel(), 2))
 local userName = string.sub(os.getComputerLabel(), 2)
 
 -- Remote URLs
-local remoteUrl = "http://dev.otegamers.com/helkarakse/tps/upload.php?req=get&format=json&dim=" .. dimId
+local remoteUrl = "http://dev.otegamers.com/helkarakse/index.php?c=upload&m=get&server=".. server .. "&type=1" .. dimId
 local backupUrl = "http://dev.otegamers.com/helkarakse/tps/backup.php?name=" .. userName .. "&dim=" .. dimId
 local authUrl = "http://dev.otegamers.com/helkarakse/ticket/auth.php?cmd=get_auth"
 
