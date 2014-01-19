3d55ea01908572bc6c9369d004aee5ac581c7bd2
Shotexpert <sandergillissen@hotmail.com>
2014-01-19 14:27:51 +0100

    fixing lines derpyness

--- src/glass.lua
+++ src/glass.lua
@@ -423,14 +423,14 @@ local function drawHelp(inputX, inputY)
 	local helpArray = {}
 
 	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 0), "Available commands (all commands use $$show <variable>): ", configArray.textColor.value).setScale(size.small))
-	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 1), "$$show mini -- Brings you to the minimalized screen.", configArray.textColor.value).setScale(size.small))
-	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 2), "$$show tps -- Brings you to the tps screen.", configArray.textColor.value).setScale(size.small))
-	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 3), "$$show rss -- Brings you to the rrs screen.", configArray.textColor.value).setScale(size.small))
-	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 3), "$$show help -- Brings you to the help screen.", configArray.textColor.value).setScale(size.small))
-	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 3), "$$show themes -- Brings you to the themes screen.", configArray.textColor.value).setScale(size.small))
-	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 3), "$$show options -- Brings you to the options screen.", configArray.textColor.value).setScale(size.small))
-	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 3), "$$show hide -- Hides the interface.", configArray.textColor.value).setScale(size.small))
-	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 3), "$$show -- Shows the interface.", configArray.textColor.value).setScale(size.small))
+	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 2), "$$show mini -- Brings you to the minimalized screen.", configArray.textColor.value).setScale(size.small))
+	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 3), "$$show tps -- Brings you to the tps screen.", configArray.textColor.value).setScale(size.small))
+	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 4), "$$show rss -- Brings you to the rrs screen.", configArray.textColor.value).setScale(size.small))
+	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 5), "$$show help -- Brings you to the help screen.", configArray.textColor.value).setScale(size.small))
+	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 6), "$$show themes -- Brings you to the themes screen.", configArray.textColor.value).setScale(size.small))
+	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 7), "$$show options -- Brings you to the options screen.", configArray.textColor.value).setScale(size.small))
+	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 8), "$$show hide -- Hides the interface.", configArray.textColor.value).setScale(size.small))
+	tableInsert(helpArray, bridge.addText(inputX, inputY + (lineMultiplier * 9), "$$show -- Shows the interface.", configArray.textColor.value).setScale(size.small))
 
 	for i = 1, #helpArray do
 		helpArray[i].setZIndex(5)
